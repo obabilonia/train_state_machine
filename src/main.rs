802d1f2e043bc5a2b986bd930292ccd09dea00f8
Oscar Andres Babilonia Perez <oscar-babilonia@hotmail.com>
2025-11-09 11:27:36 -0300

    implement first block

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,22 @@ impl Runtime {
 
 
 fn main() {
+    let mut runtime = Runtime::new();
+    runtime.balances.set_balance(&"alice".to_string(), 100);
+
+    // start emulating a block
+    runtime.system.inc_block_number();
+    assert_eq!(runtime.system.block_number(), 1);
+
+    // first transaction
+    runtime.system.inc_nonce(&"alice".to_string());
+    let _res = runtime.balances.transfer("alice".to_string(), "bob".to_string(), 30).map_err(|e| println!("Transaction failed: {}", e));
+
+    // second transaction
+    runtime.system.inc_nonce(&"alice".to_string());
+    let _res = runtime.balances.transfer("alice".to_string(), "charlie".to_string(), 20).map_err(|e| println!("Transaction failed: {}", e));
+
+
     println!("Hello, world!");
 }
 
